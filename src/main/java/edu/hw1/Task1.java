0f8d13faa563d0dc38b6301a0634fc8e08cf2621
Ilia Popov <popov02.ia@gmail.com>
2023-10-10 00:07:08 +0300

    hw1: Task1 and Task1Test have been updated

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,29 +1,29 @@
 package edu.hw1;
 
 public class Task1 {
+    final static int MAX_SECONDS = 60;
+
+    private Task1() {
+        throw new UnsupportedOperationException();
+    }
+
     public static int minutesToSeconds(String input) {
-        try {
-            if (input.length() > 13 || !input.contains(":")) {
-                return -1;
-            }
-
+        if (input.contains(":")) {
             String[] minutesAndSeconds = input.split(":");
-            if (minutesAndSeconds[1].length() != 2) {
-                return -1;
+            if (minutesAndSeconds[1].length() == 2) {
+                int minutes, seconds;
+                try {
+                    minutes = Integer.parseInt(minutesAndSeconds[0]);
+                    seconds = Integer.parseInt(minutesAndSeconds[1]);
+                } catch (NumberFormatException e) {
+                    return -1;
+                }
+                if (minutes >= 0 && seconds >= 0
+                && seconds < MAX_SECONDS && ((Integer.MAX_VALUE - seconds) / MAX_SECONDS) >= minutes) {
+                        return minutes * MAX_SECONDS + seconds;
+                }
             }
-
-            int minutes = Integer.parseInt(minutesAndSeconds[0]);
-            int seconds = Integer.parseInt(minutesAndSeconds[1]);
-            if (minutes < 0 || seconds < 0) {
-                return -1;
-            }
-            if (seconds >= 60 || ((Integer.MAX_VALUE - seconds) / 60) < minutes) {
-                return -1;
-            }
-
-            return minutes * 60 + seconds;
-        } catch (NumberFormatException e) {
-            return -1;
         }
+        return -1;
     }
 }
--- src/test/java/edu/hw1/Task1Test.java
+++ src/test/java/edu/hw1/Task1Test.java
@@ -1,78 +1,94 @@
 package edu.hw1;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Task1Test {
     @Test
-    @DisplayName("Длина видео в секундах")
-    void minutesToSeconds() {
-        // given duration without seconds
-        String videoDuration = "01:00";
+    void minutesToSecondsFirst() {
+        // given seconds = 0
+        String videoDuration = "10:00";
 
         // when
         int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
-            .isEqualTo(60);
+            .isEqualTo(600);
+    }
 
-        // given duration without minutes
-        videoDuration = "00:13";
+    @Test
+    void minutesToSecondsSecond() {
+        // given minutes = 0
+        String videoDuration = "00:13";
 
         // when
-        durationToSeconds = Task1.minutesToSeconds(videoDuration);
+        int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
             .isEqualTo(13);
+    }
 
-        // given duration with minutes and seconds
-        videoDuration = "13:56";
+    @Test
+    void minutesToSecondsThird() {
+        // given
+        String videoDuration = "13:56";
 
         // when
-        durationToSeconds = Task1.minutesToSeconds(videoDuration);
+        int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
             .isEqualTo(836);
+    }
 
-        //given (seconds >= 60)
-        videoDuration = "10:60";
+    @Test
+    void minutesToSecondsFourth() {
+        // given seconds >= MAX_SECONDS
+        String videoDuration = "10:60";
 
         // when
-        durationToSeconds = Task1.minutesToSeconds(videoDuration);
+        int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
             .isEqualTo(-1);
+    }
 
-        // given (minutes < 0 || seconds < 0)
-        videoDuration = "-5:15";
+    @Test
+    void minutesToSecondsFifth() {
+        // given minutes < 0 || seconds < 0
+        String videoDuration = "-5:15";
 
         // when
-        durationToSeconds = Task1.minutesToSeconds(videoDuration);
+        int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
             .isEqualTo(-1);
+    }
 
+    @Test
+    void minutesToSecondsSixth() {
         // given (minutes * 60 + seconds) > Integer.MAX_VALUE
-        videoDuration = "300000000:15";
+        String videoDuration = "300000000:15";
 
         // when
-        durationToSeconds = Task1.minutesToSeconds(videoDuration);
+        int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
             .isEqualTo(-1);
+    }
 
+    @Test
+    void minutesToSecondsSeventh() {
         // given invalid string
-        videoDuration = "16.40";
+        String videoDuration = "16.40";
 
         // when
-        durationToSeconds = Task1.minutesToSeconds(videoDuration);
+        int durationToSeconds = Task1.minutesToSeconds(videoDuration);
 
         // then
         assertThat(durationToSeconds)
