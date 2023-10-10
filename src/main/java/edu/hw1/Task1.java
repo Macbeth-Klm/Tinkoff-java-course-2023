d1c77c310fbda653eec32613ab6f4d7bae486464
Ilia Popov <popov02.ia@gmail.com>
2023-10-10 13:50:52 +0300

    hw1: Task1.java has been simplified

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -10,18 +10,17 @@ public final class Task1 {
         if (input.contains(":")) {
             String[] minutesAndSeconds = input.split(":");
             if (minutesAndSeconds[1].length() == 2) {
-                int minutes;
-                int seconds;
                 try {
-                    minutes = Integer.parseInt(minutesAndSeconds[0]);
-                    seconds = Integer.parseInt(minutesAndSeconds[1]);
+                    int minutes = Integer.parseInt(minutesAndSeconds[0]);
+                    int seconds = Integer.parseInt(minutesAndSeconds[1]);
+                    if (minutes >= 0 && seconds >= 0
+                        && seconds < MAX_SECONDS && ((Integer.MAX_VALUE - seconds) / MAX_SECONDS) >= minutes) {
+                        return minutes * MAX_SECONDS + seconds;
+                    }
                 } catch (NumberFormatException e) {
                     return -1;
                 }
-                if (minutes >= 0 && seconds >= 0
-                    && seconds < MAX_SECONDS && ((Integer.MAX_VALUE - seconds) / MAX_SECONDS) >= minutes) {
-                    return minutes * MAX_SECONDS + seconds;
-                }
+
             }
         }
         return -1;
