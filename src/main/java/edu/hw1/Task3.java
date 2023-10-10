6b2ac0d5a454c6b35125ae4bbc55c2b80b093405
Ilia Popov <popov02.ia@gmail.com>
2023-10-10 13:42:33 +0300

    hw1: Task3.java has been simplified

--- src/main/java/edu/hw1/Task3.java
+++ src/main/java/edu/hw1/Task3.java
@@ -5,26 +5,21 @@ public final class Task3 {
     }
 
     public static boolean isNestable(int[] a1, int[] a2) {
-        int min1 = a1[0];
-        int max1 = a1[0];
-        int min2 = a2[0];
-        int max2 = a2[0];
+        int min = a1[0];
+        int max = a1[0];
         for (int el : a1) {
-            if (el < min1) {
-                min1 = el;
+            if (el < min) {
+                min = el;
             }
-            if (el > max1) {
-                max1 = el;
+            if (el > max) {
+                max = el;
             }
         }
         for (int el : a2) {
-            if (el < min2) {
-                min2 = el;
-            }
-            if (el > max2) {
-                max2 = el;
+            if (min > el || max < el) {
+                return true;
             }
         }
-        return (min1 > min2 || max1 < max2);
+        return false;
     }
 }
