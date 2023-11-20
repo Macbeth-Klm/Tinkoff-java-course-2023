1241c93ff01504e1852ba45892647105aca52443
Ilia Popov <popov02.ia@gmail.com>
2023-11-20 23:52:11 +0300

    hw6: second attempt has been finalised

--- src/test/java/edu/hw6/task5/HackersNewsTest.java
+++ src/test/java/edu/hw6/task5/HackersNewsTest.java
@@ -9,7 +9,7 @@ class HackersNewsTest {
     void shouldReturnHackerNewsTopStories() {
         long[] id = HackersNews.hackerNewsTopStories();
         assertThat(id)
-            .hasSize(493);
+            .hasSize(498);
     }
 
     @Test
