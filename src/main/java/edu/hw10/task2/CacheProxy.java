1abfbe70ecd159d3289dda3c3f37e5a1f32fcf1c
Ilia Popov <popov02.ia@gmail.com>
2023-12-11 22:00:14 +0300

    hw10: added memory cache if persist = false, and also added another interface to check if memory cache is working

--- src/main/java/edu/hw10/task2/CacheProxy.java
+++ src/main/java/edu/hw10/task2/CacheProxy.java
@@ -10,9 +10,12 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CacheProxy implements InvocationHandler {
     private final Object target;
+    private static final Map<String, Object> MEMORY_CACHE = new HashMap<>();
 
     private CacheProxy(Object target) {
         this.target = target;
@@ -30,25 +33,37 @@ public class CacheProxy implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         var cacheAnnotation = method.getAnnotation(Cache.class);
-        if (cacheAnnotation != null && cacheAnnotation.persist()) {
-            String cacheKey = method.getName() + Arrays.toString(args);
-            File cacheFile = new File("src/main/java/edu/hw10/task2/cache/" + cacheKey + ".txt");
-            if (cacheFile.exists()) {
-                try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(cacheFile))) {
-                    return in.readObject();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+        if (cacheAnnotation != null) {
+            Object result;
+            String cacheKey = method.getName() + Arrays.deepToString(args);
+            if (cacheAnnotation.persist()) {
+                File cacheFile = new File("src/main/java/edu/hw10/task2/cache/" + cacheKey + ".txt");
+                if (cacheFile.exists()) {
+                    try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(cacheFile))) {
+                        result = in.readObject();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    result = method.invoke(target, args);
+                    try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(cacheFile))) {
+                        out.writeObject(result);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+            } else if (MEMORY_CACHE.containsKey(cacheKey)) {
+                result = MEMORY_CACHE.get(cacheKey);
             } else {
-                Object result = method.invoke(target, args);
-                try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(cacheFile))) {
-                    out.writeObject(result);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                return result;
+                result = method.invoke(target, args);
+                MEMORY_CACHE.put(cacheKey, result);
             }
+            return result;
         }
         return method.invoke(target, args);
+    }
+
+    public static Map<String, Object> getMemoryCache() {
+        return MEMORY_CACHE;
     }
 }
--- src/test/java/edu/hw10/task2/CacheProxyTest.java
+++ src/test/java/edu/hw10/task2/CacheProxyTest.java
@@ -4,15 +4,19 @@ import edu.hw10.task2.concat.ConcatterClass;
 import edu.hw10.task2.concat.StringConcatter;
 import edu.hw10.task2.fib.FibCalculator;
 import edu.hw10.task2.fib.FibClass;
+import edu.hw10.task2.sum.SumIntegers;
+import edu.hw10.task2.sum.SumIntegersClass;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CacheProxyTest {
@@ -88,13 +92,42 @@ class CacheProxyTest {
         }
         StringConcatter concatter = new ConcatterClass();
         StringConcatter proxy = CacheProxy.create(concatter, StringConcatter.class);
-
-        String result = proxy.concat("My name is", " Ilia");
+        String result = proxy.concat("Hello", " World!");
+        String anotherResult = proxy.concat("Hello", " World!"); // Проверка, что не создаётся ещё один файл
         long newFilesCount;
         try (var pathStream = Files.list(DIRECTORY)) {
             newFilesCount = pathStream.count();
         }
 
-        assertTrue(result.equals("My name is Ilia") && newFilesCount == filesCount + 1);
+        assertTrue(
+            result.equals("Hello World!")
+                && anotherResult.equals(result)
+                && newFilesCount == filesCount + 1
+        );
+    }
+
+    @Test
+    void
+    shouldReturnCorrectSumAndAddResultInMemoryCacheWithoutCreatingFileBecausePersistIsFalse() throws IOException {
+        long filesCount;
+        try (var pathStream = Files.list(DIRECTORY)) {
+            filesCount = pathStream.count();
+        }
+        SumIntegers sumIntegers = new SumIntegersClass();
+        SumIntegers proxy = CacheProxy.create(sumIntegers, SumIntegers.class);
+        int result = proxy.sum(1, 2, 3, 4);
+        long newFilesCount;
+        try (var pathStream = Files.list(DIRECTORY)) {
+            newFilesCount = pathStream.count();
+        }
+
+        assertTrue(
+            result == 10
+                && newFilesCount == filesCount
+        );
+        assertThat(CacheProxy.getMemoryCache())
+            .containsExactlyInAnyOrderEntriesOf(Map.of(
+                "sum[[1, 2, 3, 4]]", 10
+            ));
     }
 }
